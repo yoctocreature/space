583d0d0ddf19c2299db8e317bb7e9eecada44eb4
yoctocreature <yocto.creature@gmail.com>
2025-12-03 14:06:37 +0900

    Add files via upload

---
+++ images.js
@@ -0,0 +1,213 @@
+
+// List of images/videos/GIFs
+const images = [
+{ src: "https://lh3.googleusercontent.com/d/1Y7meayhzN7M3Oy0DrVGiiOEDvYtFDh2a=w1200",
+        alt: "DTA App",
+        categories: ["product design", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1j5qKxY-NJVM0dgIREv00jfPGrFuFK2Ey=w1200", "https://lh3.googleusercontent.com/d/1b01BD7GA7y1bbSf_aWEnE8WTX8aaG6xm=w1200"],
+        text: "Type: Phone Application \nFunction: Developer \n\nDTA: Digital Traveling Agency redefines global exploration. It connects you to a network of real-world travelers, delivering real-time experiences directly. DTA utilizes global Local Explorers for micro-trips—hiring them for personalized, live-streamed tours of their immediate area (e.g., a Parisian coffee shop). Alternatively, become a Global Contributor to fund DTA Travelers on macro-journeys (like backpacking Asia), receiving exclusive content and influencing their travels with real-time tips. DTA is a social network for genuine, real-time exploration. You gain the experience; they gain support. Travel the world without moving."
+    },
+{ src: "https://lh3.googleusercontent.com/d/1It6_fexSflYCfj_h6NhbUvr6yZZkVF2Y=w1200",
+        alt: "MAU",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "MAU (마우), a Korean footwear brand, specializes in comfortable, lightweight, and stylish casual sneakers. Their core philosophy, driven by a visually clean, contemporary aesthetic, centers on feather-light comfort and modern, minimalist design for everyday wear. Emphasizing simplicity, versatility, and ergonomic design, MAU shoes feature a unique 'MAU SOLE' and breathable materials in subtle, versatile tones, enhancing your look with sophisticated elegance and a natural walking feel."
+
+    },
+
+   { src: "https://lh3.googleusercontent.com/d/1PvqBPlD0JysbDoav79Zoa0SeXRkVCH95=w1200",
+        alt: "Think",
+        categories: ["print", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "3 - This is the custom text that will appear in the lightbox."
+    },
+        
+{
+        src: "https://lh3.googleusercontent.com/d/1-IfFsOem4_uFoWHL7x0gUz-Nkk8s-Bu8=w1200",
+        alt: "30DII",
+        categories: ["print"],
+        group: ["https://lh3.googleusercontent.com/d/15FTIUfeVcqKgX-D7WNouuUOAzcj-2VRC=w1200",
+            "https://lh3.googleusercontent.com/d/1kEALBwWjwUwCIXXtLOPIFRjghW6zi3gD=w1200",
+            "https://lh3.googleusercontent.com/d/1eGDP41pcqUYDwj1IMc4LDK_IY5lMxOUj=w1200",
+            "https://lh3.googleusercontent.com/d/1o9Yoe_mLvZtMifItUwLA02WHEtrz-oss=w1200",
+            "https://lh3.googleusercontent.com/d/1FZkyoX1s0bDIRWeEHrgwfNtbylAyjZzS=w1200",
+            "https://lh3.googleusercontent.com/d/1rbu6THSuW3DzBIBJXI0MmVY48Zpjsbjy=w1200",
+            "https://lh3.googleusercontent.com/d/1ky8YgZzDVj4IVouvlf8YLbRjOJimsqn-=w1200",
+            "https://lh3.googleusercontent.com/d/1hkt52J-1Ec2temdDGcoGBzZaicaoiO2n=w1200"],
+        text: "30DII (Thirty Days Immediate Inference) is not merely a project; it is a crucible of creativity. Over the span of a singular, intense month, artists spanning the globe are drawn into a shared, accelerated current. Each day unfurls a new task, a concentrated pulse challenging their immediate intuition and craft. These urgent responses—these visual murmurs and written whispers—are then meticulously woven into a singular, tactile volume: a book blooming with both evocative imagery and incisive prose.The core inquiry of 30DII lies in this deliberate compression: to sculpt art within a pressure cooker, to witness the raw, divergent bloom of creation when time, that most relentless of muses, demands an immediate answer."
+  },
+
+{
+        src: "https://lh3.googleusercontent.com/d/1NhuBTVYigNda4gX7SjaG1AnYxWhwlfoe=w1200",
+        alt: "Space Invaders",
+        categories: ["print", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "Space Invaders, the iconic 1978 arcade phenomenon, specializes in delivering relentless alien invasion and groundbreaking electronic challenge. Its core philosophy, driven by a visually simple, pixelated aesthetic, centers on addictive gameplay and escalating tension for endless replayability. Emphasizing immediate action, strategic cover, and iconic sound, Space Invaders features descending alien formations and laser cannons in a stark, unforgettable battleground, enhancing your reflexes with timeless thrills and a foundational gaming experience."
+    },
+  
+  {
+        src: "https://lh3.googleusercontent.com/d/1PXKgsT4wJTHYtTM6AEO2JrviddGzrMlH=w1200",
+        alt: "Jolly Roger, Waves The White Flag",
+        categories: ["3d"],
+        group: ["https://lh3.googleusercontent.com/d/1z5hlLZvZ7E89QKkdwBOegW3u45UeAJqx=w1200",
+            "https://lh3.googleusercontent.com/d/1Q6AyJgcSu3-xw_O13a8bnkl_QyzhelBz=w1200",
+            "https://lh3.googleusercontent.com/d/1mMb37RFB4RQGqMT5fSKAE4F4wnL8RpW1=w1200",
+            "https://lh3.googleusercontent.com/d/1v-qeoTEP0I1Vq0XCyH_FyVOKF4ILfqfR=w1200",
+            "https://lh3.googleusercontent.com/d/14e7R4cbk4nc7pmW5EXUKt16GlX3auofQ=w1200",
+            "https://lh3.googleusercontent.com/d/1rHPUDFseJP4AEG13pNXGXgOvaN-zBWmx=w1200" ],
+        text: "Jolly Roger, Waves the White Flag is not merely a collection of civic emblems; it is a crucible of profound cultural inquiry. Over the span of its singular, intense exploration, this book draws the reader into a shared, accelerated current, where each state flag unfurls a new task—a concentrated pulse challenging our immediate intuition about national identity and its aesthetic codification. These urgent responses—these visual murmurs and symbolic whispers of history—are then meticulously woven into a singular, tactile volume: a book blooming with both evocative imagery and incisive prose. The core inquiry of Jolly Roger, Waves the White Flag lies in this deliberate compression: to sculpt meaning within the pressure cooker of vexillological critique, to witness the raw, divergent bloom of civic self-perception when history, that most relentless of muses, demands an immediate answer regarding the banners we fly."
+    },
+  
+{
+        src: "https://lh3.googleusercontent.com/d/1xhY84kuP02c1qq7ahx5Q44KfFfU-bLJu=w1200",
+        alt: "PLAUAT App",
+        categories: ["product design", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "7 - This is the custom text that will appear in the lightbox."
+    },
+        
+     {src: "https://lh3.googleusercontent.com/d/1ImWqmE7xA0CBkEe3uJnudWVErv_zBnCP=w1200",
+        alt: "Kinx",
+        categories: ["print", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "KINX, a leading Internet eXchange (IX) provider and data center operator in Korea, offers critical backbone infrastructure. Serving ISPs, content, cloud providers, and enterprises, not consumers, KINX emphasizes extensive domestic and international network connections. As Korea's intelligent internet backbone, KINX delivers seamless, direct interconnection at its neutral IX, accelerating network performance. Beyond bandwidth, KINX guarantees unwavering stability and continuous operation with robust, redundant data center and IX services, securing your digital foundation with resilient infrastructure engineered for maximum uptime and peace of mind."
+    },
+
+{
+        src: "https://lh3.googleusercontent.com/d/1I7S1LB7e2FaIMDz9Zz6Fm5Ipwug2uqmF=w1200",
+        alt: "Kiiroo",
+        categories: ["product"],
+        group: ["https://lh3.googleusercontent.com/d/1MHFNJLvORYda51aFe-JtrivM-k4TcXG-=w1200"],
+        text: "KIIROO pioneers adult entertainment technology, developing remote devices for synchronised intimate experiences. Their innovation transmits real-time touch sensations, often synced with video or other users, to bridge the physical gap in relationships. KIIROO empowers users to explore new dimensions of virtual intimacy, whether solo, with a partner, or with content creators, making connections feel more real and enhancing sexual experiences through technology."
+    },
+ 
+{
+        src: "https://lh3.googleusercontent.com/d/1AdUvGtMf8am8vdEvKlYeGbiECrhO24KA=w1200",
+        alt: "Glass_tooth",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "10 - This is the custom text that will appear in the lightbox."
+    },
+        
+ {
+        src: "https://lh3.googleusercontent.com/d/1LHJU0gas_N5T2cZbtnIcpU3ySLtd6drf=w1200",
+        alt: "Jerrican",
+        categories: ["print", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "Jerrican eyewear elevates your style with contemporary design and timeless elegance. Each meticulously crafted frame offers unparalleled clarity and flair, engineered for lightweight comfort, exceptional durability, and a perfect everyday fit. Discover Jerrican: eyewear that truly reflects your ambition, leisure, and unmistakable charm. See the style. Feel the difference."
+    },
+  
+ {
+        src: "https://lh3.googleusercontent.com/d/1eHsnGo3XG8R9-gItEXHnWNNzkwf4M9Gy=w1200",
+        alt: "Guns'N'Roses",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "Guns N' Roses, the iconic American hard rock band of the late '80s, soared to fame with Axl Rose, Slash, and Duff McKagan, delivering raw energy and anthems like 'Sweet Child o' Mine.' Their 'Appetite for Destruction' remains a best-seller, solidifying their legendary status. Fans can rejoice as the band will embark on a highly anticipated 2026 tour, promising another high-octane spectacle, delivering timeless hits and fierce energy to their global fanbase."
+    },
+     {
+        src: "https://lh3.googleusercontent.com/d/1emihWoLZYVphvLgbJI8iJ5udMeI93F0Y=w1200",
+        alt: "Nike",
+        categories: ["digital"],
+          group: ["https://player.vimeo.com/video/1141425366?background=1&autoplay=1&loop=1&muted=1"],
+
+        text: "The Nike x Louis Vuitton collaboration, driven by Virgil Abloh, fused luxury fashion with street culture. Abloh reimagined iconic Nike Air Force 1s with Louis Vuitton craftsmanship, released posthumously. His tragic loss left a profound void, but this partnership stands as a powerful, final testament to his genius and unparalleled impact on design, blending aesthetics and challenging conventions."
+    },
+
+  {
+        src: "https://lh3.googleusercontent.com/d/1jt1a1GyDPq9OeYrgfxcVIXMTK3md0Sbl=w1200",
+        alt: "Next Rise",
+        categories: ["product design", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "NextRise is South Korea's major startup conference, designed as a comprehensive platform. It connects startups with investors, corporations, and media to foster innovation, facilitate investment, and promote global collaboration. This event helps ventures, corporate investors, and other innovation stakeholders forge partnerships, secure funding, and gain unparalleled insights, serving as a gateway to the future of innovation."
+    },
+
+ {
+        src: "https://lh3.googleusercontent.com/d/1H8_lRyafliGnAOtn20LoAbxz8h3Ztz0n=w1200",
+        alt: "Artinus",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1FytNOBzF0bw0S6d_gKEQ-1dClOrSR5re=w1200",
+                "https://lh3.googleusercontent.com/d/1zz4wqKeLOYuZhZlbisOi9TxBmMk_OS-p=w1200",
+                "https://lh3.googleusercontent.com/d/19ADZm7PbqWLpa8gxWnMokGWQO_NJCa16=w1200",
+                "https://lh3.googleusercontent.com/d/1Ebf65QNHchQq-f7WTDgwRprC05RNEihY=w1200",
+                "https://lh3.googleusercontent.com/d/11ufqfJGMWDLxoFRRqKKUStFEJfsFma_v=w1200",
+                "https://lh3.googleusercontent.com/d/1sE0uhr9OvYCG9nkugEu0fO-bRta9UrhC=w1200"],
+        text: "Artinus is a coffee appliance brand created through a collaboration between the Italian espresso machine manufacturer Officine Brugnetti and the South Korean coffee company JM Coffee Group. The brand focuses on high-end espresso machines and related accessories, aiming to combine Italian tradition and innovation with Korean design and technological needs."
+    },
+
+ 
+{
+        src: "https://lh3.googleusercontent.com/d/1c-H35RgNXWWiEODJMmWdXJYiuzSNBZVx=w1200",
+        alt: "Sit App",
+        categories: ["product design", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1Ju2KsoA5I0OlfOrzHaRF5akVRP1tVzhX=w1200",
+            "https://lh3.googleusercontent.com/d/1JQHTK-jCUzwsI-y8GUysUdD_UtffmBgQ=w1200",
+            "https://lh3.googleusercontent.com/d/1IhQnt0sLuffeyHGbR3eAbPc-YXcmqeln=w1200" ],
+text: "SIT is a peer-to-peer mobile application for high-density, unreserved transit. It allows existing passengers to list their highly desired seat in real-time. Standing riders can purchase the immediate right of occupancy securely via the app. Using precise location and time tracking, SIT coordinates the exchange. Eliminate the stress of standing and guarantee comfort for the final leg. Secure, discreet payment ensures a seamless transition between riders. SIT turns an occupied spot into a guaranteed, purchasable seat"
+    },
+  
+ 
+{
+        src: "https://lh3.googleusercontent.com/d/1TK4f5gCsP7BFOEADC_S95nVC12_NWpZj=w1200",
+        alt: "Polyspot",
+        categories: ["product design", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1qnW1cFUEsYKX1RdIM5KOKpBahhViYXez=w1200",
+            "https://lh3.googleusercontent.com/d/1s_MAOrC2y8wTnnRGB-eUiq2uy1eQVCNa=w1200",
+            "https://lh3.googleusercontent.com/d/1L7bKdukUgHA5o4YoOIjUcXXyLxxz73hF=w1200",
+            "https://lh3.googleusercontent.com/d/1bib0w_76GibZlfhs1QqmXeViudPwg710=w1200" ],
+        text: "Polyspot, Spotify's iconic modern audio innovation, specializes in delivering personal music experiences and groundbreaking tactile interaction. Its core philosophy, driven by a visually clean, contemporary aesthetic, centers on intuitive control and seamless access for endless listening pleasure. Emphasizing 360-degree sound from its unique 'spots,' a vibrant circular display, and integrated Wi-Fi, Polyspot features a direct connection to Spotify's library, enhancing your daily rhythm with feather-light freedom and a foundational sound experience. Get yours free with an annual Spotify Premium subscription."
+    },
+
+  
+ { src: "https://lh3.googleusercontent.com/d/12yh7OmQPdnBEHswGDG6-6HqUXB12-zOM=w1200",
+        alt: "F*ck That Smile",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200",
+            "https://lh3.googleusercontent.com/d/1kjSC1Cq3ZJyi6uBe1CF1yWqAla9l_-d8=w1200",
+            "https://lh3.googleusercontent.com/d/1cGf6mqDp5e4s6QV0IZwA7nvfin1FuDOi=w1200",
+            "https://lh3.googleusercontent.com/d/1FEBL8nfwI6VgErRb95TB0B0vGjB7Jxu2=w1200",
+            "https://lh3.googleusercontent.com/d/1MHcfbajeA9-m3nAckdwFLcxsG-HwFz4r=w1200",
+            "https://lh3.googleusercontent.com/d/1pqqaSgQbtqtSytEhxJzlrJA0hgFDWbvt=w1200" ],
+     text: "'f*ck that smile' is a personal odyssey: a defiant, raw view of self, rejecting external happiness for intensely personal truth. The artist's promise is self-provided happiness. Set in the introspective mountains of Castlenuovo Di Garfagnana and busy Tokyo, it follows an alien stranded on Earth after a crash. This wanderer seeks belonging but finds no solace. More than music, it's a profound inner struggle—physical displacement mirroring emotional rift. Each frame echoes defiance, inviting audiences to see the difficult path to authentic self-acceptance. f*ck that smile: The only smile that matters is your own."
+    },
+
+{ src: "https://lh3.googleusercontent.com/d/1j9CGeOUhwHnYE1JNWCfQlpXl1ATJZWTh=w1200",
+        alt: "Yaksu",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "19 - This is the custom text that will appear in the lightbox."
+    },
+
+ { src: "https://lh3.googleusercontent.com/d/1YcTTIG9o4bVfpRpZZuYYuSYrFLfWHCUb=w1200",
+        alt: "Prompt",
+        categories: ["digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "Prompt is a fragmented, poetic, and philosophical exploration of themes surrounding defiance, consequence, nature's cruelty, societal rituals, and the cycles of existence. It employs repetition, rhetorical questions, and vivid, often disturbing, imagery to create a sense of unease and a questioning of established norms. The language shifts between declarative statements, narrative descriptions (the bible, the ortolan ritual, the cuckoo), and introspective or challenging questions. At times defiant, at others descriptive, and often laden with a dark, almost cynical undercurrent regarding human and natural behaviors."
+    },
+ 
+{ src: "https://lh3.googleusercontent.com/d/1citj8Zu7j3JEzF-B7kczQiYVg8n-PNqo=w1200",
+        alt: "Stella Maris",
+        categories: ["print", "digital"],
+        group: ["https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200"],
+        text: "Stella Maris elevates every diver's experience with Korean digital precision. Guiding you with timeless elegance, our watches blend celestial inspiration with masterful craftsmanship. Crafted for those who navigate life's currents with unwavering style, Stella Maris timepieces are engineered for exceptional accuracy, enduring beauty, and reborn precision."
+    },
+
+        
+{
+        src: "https://lh3.googleusercontent.com/d/1YkYFncv1MhxGUAwpY1LcoO-XM__HQp1D=w1200",
+        alt: "Ballsy",
+        categories: ["product design", "digital"],
+         group: ["https://lh3.googleusercontent.com/d/1Ju2KsoA5I0OlfOrzHaRF5akVRP1tVzhX=w1200",
+            "https://lh3.googleusercontent.com/d/1JQHTK-jCUzwsI-y8GUysUdD_UtffmBgQ=w1200",
+            "https://lh3.googleusercontent.com/d/1IhQnt0sLuffeyHGbR3eAbPc-YXcmqeln=w1200"
+        ],
+        text: "BALLSY fuels every ambition, delivering a surge of energy to elevate your potential. Guiding you to defy limits, our drink blends essential vitality with peak performance. Crafted for those who navigate life's challenges with unwavering drive, BALLSY gives you spirit for extraordinary feats, unwavering focus, and limitless inspiration."
+    },
+
+];
+
+// Function to filter images based on category
+function getFilteredImages(filter) {
+  return filter === 'all'
+    ? images
+    : images.filter(img => img.categories.includes(filter));
+}
